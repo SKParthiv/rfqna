b4b7be6be6efb6238a1178b4a5fe22c2ca760e6b
SKParthiv <parthiv.sk.pedapati@gmail.com>
2024-10-29 01:11:53 +0000

    really_hope_this_is_final

--- q6.py
+++ q6.py
@@ -22,3 +22,29 @@ for j in range(len(emp)):
     sorted_tuple += (mi,)
     mi = (emp[0][0], emp[1][1], avg*len(emp))
 print(sorted_tuple)
+
+# Output
+# Enter the Employee No.: 1
+# Enter the name of Employee No. 1: Jeff Bezoz
+# Enter the salary of Mr./Ms. Jeff Bezoz: 20000000
+# Type 'terminate' to terminate giving inputs: 
+# Enter the Employee No.: 2
+# Enter the name of Employee No. 2: Elon Musk
+# Enter the salary of Mr./Ms. Elon Musk: 90000000
+# Type 'terminate' to terminate giving inputs: 
+# Enter the Employee No.: 3
+# Enter the name of Employee No. 3: Tim Cook
+# Enter the salary of Mr./Ms. Tim Cook: 70000000
+# Type 'terminate' to terminate giving inputs: 
+# Enter the Employee No.: 4
+# Enter the name of Employee No. 4: Satya Nadela
+# Enter the salary of Mr./Ms. Satya Nadela: 60000000
+# Type 'terminate' to terminate giving inputs: 
+# Enter the Employee No.: 5
+# Enter the name of Employee No. 5: Sundar Pichai
+# Enter the salary of Mr./Ms. Sundar Pichai: 100000000
+# Type 'terminate' to terminate giving inputs: terminate
+# (2, 'Elon Musk', 90000000)
+# (3, 'Tim Cook', 70000000)
+# (5, 'Sundar Pichai', 100000000)
+# ((1, 'Jeff Bezoz', 20000000), (4, 'Satya Nadela', 60000000), (3, 'Tim Cook', 70000000), (2, 'Elon Musk', 90000000), (5, 'Sundar Pichai', 100000000))
